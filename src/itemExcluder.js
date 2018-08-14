126f7dd86b5fb7882cd3529f55a91833f955cf7f
Joe Karlsson <josephcarlson1@gmail.com>
2018-08-14 12:29:49 -0500

    finished itemExcluder.

--- src/itemExcluder.js
+++ src/itemExcluder.js
@@ -1,12 +1,53 @@
-// implement a method to effieciently exlude items from the items array, as outlined in the exlude array.
+// Implement a method to effieciently exlude items from the items array, as outlined in the exlude array.
 
-const items = [
+const itemList = [
 	{type: 'computer', color: 'green', flavor: 'apple'},
 	{type: 'cellphone', color: 'gold', flavor: 'orange'},
 	{type: 'computer', color: 'silver', flavor: 'grape'},
 ]
 
-const exclude = [
+const excludeList = [
 	{k: 'type', v: 'cellphone'},
 	{k: 'color', v: 'silver'},
+	{k: 'color', v: 'gold'},
 ];
+
+const itemExcluder = (items, exlude) => {
+	const excludeCache = {};
+
+	// Build exclude cache
+	exlude.forEach(item => {
+		if (excludeCache.hasOwnProperty(item.k)) {
+			excludeCache[item.k][item.v] = true;
+		} else {
+			const newExludeObj = {};
+			newExludeObj[item.v] = true
+			excludeCache[item.k] = newExludeObj;
+		}
+	});
+
+
+
+	// Check if any items are in the xclude cache
+	const filteredItems = [];
+
+	items.forEach(item => {
+		let filterFlag = false;
+
+		Object.keys(item).forEach(itemKey => {
+			if (excludeCache[itemKey]) {
+				if (excludeCache[itemKey][item[itemKey]] ) {
+					filterFlag = true
+				}
+			}
+		});
+		if (!filterFlag) {
+			return filteredItems.push(item);
+		}
+	});
+
+	return filteredItems;
+}
+
+const result = itemExcluder(itemList, excludeList);
+console.log('result', result);

bf50217589c01ac5583e9558243e9bd4d4ef03d7
Joe Karlsson <josephcarlson1@gmail.com>
2018-11-14 12:25:02 -0600

    updated palindrome solution.

--- src/isApproxPalindrome.js
+++ src/isApproxPalindrome.js
@@ -1,7 +1,11 @@
 // Determine if a string of length S is a palindrome if you remove at most a single characters from that string.
 
 //Best Case: O(1) & Worst Case: O(2N)
-const isApproxPalindrome = (str, calledOnce = false) => {
+const isPalindromeIfNLettersAreRemoved = (
+  str,
+  maxNumbersOfLetters = 1,
+  lettersRemoved = 0
+) => {
   if (str.length === 0) {
     return true;
   }
@@ -11,20 +15,27 @@ const isApproxPalindrome = (str, calledOnce = false) => {
     return true;
   }
 
-  if (calledOnce) {
+  if (lettersRemoved >= maxNumbersOfLetters) {
     return false;
   }
 
   const { badIndex } = result;
   str = str.slice(0, badIndex) + str.slice(badIndex + 1);
+  lettersRemoved++;
 
-  return isApproxPalindrome(str, true);
+  return isPalindromeIfNLettersAreRemoved(
+    str,
+    maxNumbersOfLetters,
+    lettersRemoved
+  );
 };
 
 // O(1) Space & O(N) time
 const isPalindrome = str => {
   const strArr = str.split("");
-  const strLength = str.length;
+  const strLength = strArr.length;
+  const halfLength = Math.ceil(strArr.length / 2);
+
   let result = {
     isPalindrome: true,
     badIndex: null
@@ -39,11 +50,15 @@ const isPalindrome = str => {
       };
     }
   });
+
   return result;
 };
 
-const data = ["abca", "aba", "abcd", "acba", ""]; // true, true, false, true, true
+const data = ["abca", "aba", "abcd", "acba", "", "racecafr"]; // true, true, false, true, true, true
 
 data.forEach(testCase => {
-  console.log("isApproxPalindrome(testCase)", isApproxPalindrome(testCase));
+  console.log(
+    `isApproxPalindrome($(testCase))`,
+    isPalindromeIfNLettersAreRemoved(testCase)
+  );
 });
